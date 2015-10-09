171af899fdf1251967fe594a9709c19d80ae32a4
YANG Xudong <xudong.yang@hde.co.jp>
2015-10-09 12:51:18 +0900

    Add delete queue management.

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ var sqs = new aws.SQS();
 var queueUrl = 'https://sqs.ap-northeast-1.amazonaws.com/164201395711/xudong-nobita-mailstat-test';
 
 var poll = require('./lib/poll.js');
-
-var stats = {};
+var sum = require('./lib/sum.js');
+var del = require('./lib/delete.js');
 
 exports.handler = function(event, context) {
   var source = poll.messages(sqs, queueUrl);
@@ -16,30 +16,24 @@ exports.handler = function(event, context) {
 
   var observer = Rx.Observer.create(function(msg) {
     count++;
-    add(msg, stats);
+
+    var body = JSON.parse(msg.Body);
+    sum.add(body);
+    del.append(body.domain, msg.MessageId);
   }, function(e) {
     console.log(e);
     context.done(e);
   }, function() {
+    console.log('Receive API count: ' + poll.fetchCount);
     console.log('Fetched messages: ' + count);
+
     console.log('Statistics: ');
-    console.log(stats);
+    console.log(sum.stats);
+
+    console.log('Message IDs: ');
+    console.log(del.ids);
     context.done();
   });
 
   var subscription = source.take(1000).subscribe(observer);
-};
-
-var add = function(msg, stats) {
-  var body = JSON.parse(msg.Body);
-  var domain = body.domain;
-  if (!(domain in stats)) {
-    stats[domain] = {
-      count: 0,
-      size: 0
-    };
-  }
-
-  stats[domain].count += body.count;
-  stats[domain].size += body.size;
 };
