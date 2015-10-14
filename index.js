27510bcd59b03e1646f30ddfd57da7aaca5dac4a
YANG Xudong <xudong.yang@hde.co.jp>
2015-10-14 14:38:20 +0900

    Update modules.

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ aws.config.update({
   region: 'ap-northeast-1'
 });
 
-var poll = require('./lib/poll.js');
-var sum = require('./lib/sum.js');
-var update = require('./lib/update.js');
-var del = require('./lib/delete.js');
+var Poll = require('./lib/poll.js');
+var Sum = require('./lib/sum.js');
+var Update = require('./lib/update.js');
+var Del = require('./lib/delete.js');
 
 var sqs = new aws.SQS();
 var db = new aws.DynamoDB();
@@ -18,6 +18,11 @@ var queueUrl;
 var table;
 
 var done;
+
+var poll;
+var sum;
+var update;
+var del;
 
 const job = 'Lambda Job';
 
@@ -30,7 +35,8 @@ function before(event, context) {
   console.time(job);
   console.info('Job start!');
 
-  done = prepareDone(context);
+  prepareSingletons();
+  prepareDone(context);
 
   if (!event.messageCount || !event.table || !event.queueUrl) {
     done(new Error('Event is malformed.'));
@@ -43,13 +49,20 @@ function before(event, context) {
 }
 
 function prepareDone(context) {
-  return function(error, result) {
+  done = function(error, result) {
     if (error) {
       console.error(error, error.stack);
     }
     console.timeEnd(job);
     context.done(error, result);
   };
+}
+
+function prepareSingletons() {
+  poll = Poll();
+  sum = Sum();
+  update = Update();
+  del = Del();
 }
 
 function start(context) {
@@ -60,16 +73,16 @@ function start(context) {
   }, function(e) {
     done(e);
   }, function() {
-    console.info('Receive API count: ' + poll.fetchCount);
-    console.info('Fetched messages: ' + poll.messageCount);
+    console.info('Receive API count: ' + poll.fetchCount());
+    console.info('Fetched messages: ' + poll.messageCount());
 
     var results = Rx.Observable.forkJoin(updateAndDelete());
     results.subscribe(function() {}, function(e) {
       done(e);
     }, function() {
-      console.info("Update API count: " + update.updateCount);
-      console.info("Delete API count: " + del.deleteCount);
-      console.info("Delete Message count: " + del.messageCount);
+      console.info("Update API count: " + update.updateCount());
+      console.info("Delete API count: " + del.deleteCount());
+      console.info("Delete Message count: " + del.messageCount());
 
       done(null, 'Lambda job finished successfully.');
     });
@@ -80,8 +93,9 @@ function start(context) {
 
 function updateAndDelete() {
   var results = [];
-  for (var domain in sum.stats) {
-    var stats = sum.stats[domain];
+  var allStats = sum.stats();
+  for (var domain in allStats) {
+    var stats = allStats[domain];
     var updateResult = update.exec(db, table, stats);
     var delResult = updateResult.then(function(ids) {
       return new Promise(function(resolve, reject) {
